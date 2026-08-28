d4715f68e4cc800d3f834eb5255388f7c947bc4a
dogaha <ekhangv@gmail.com>
2026-08-28 17:43:33 -0500

    Added ec2-s3 access policy

--- infra/iam.tf
+++ infra/iam.tf
@@ -19,3 +19,26 @@ resource "aws_iam_role" "ec2" {
         Environment = "dev"
     }
 }
+
+resource "aws_iam_role_policy" "ec2_s3" {
+    name = "motorsport-ec2-s3-access"
+    role = aws_iam_role.ec2.id
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = [
+                    "s3:GetObject",
+                    "s3:PutObject",
+                    "s3:ListBucket"
+                ]
+                Resource = [
+                    "arn:aws:s3:::motorsport-data-lake",
+                    "arn:aws:s3:::motorsport-data-lake/*"
+                ]
+            }
+        ]
+    })
+}
