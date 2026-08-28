dc6a4518f379b2308aa9ed6be15a60e3e299c9d0
dogaha <ekhangv@gmail.com>
2026-08-28 15:14:34 -0500

    Add VPC and subnets

---
+++ infra/networking.tf
@@ -0,0 +1,33 @@
+resource "aws_vpc" "main"{
+    cidr_block          = "10.0.0.0/16"
+    enable_dns_support  = true
+    enable_dns_hostnames = true
+    
+    tags = {
+        Project     = "motorsport"
+        Environment = "dev"
+    }
+}
+
+resource "aws_subnet" "public" {
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = "10.0.1.0/24"
+    availability_zone       = "us-east-2a"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Project     = "motorsport"
+        Environment = "dev"
+    }
+}
+
+resource "aws_subnet" "private" {
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = "10.0.2.0/24"
+    availability_zone       = "us-east-2a"
+
+    tags = {
+        Project     = "motorsport"
+        Environment = "dev"
+    }
+}
