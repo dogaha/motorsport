cc4f1dd05b5735173c3aa0f0161971bdeba2aa7a
dogaha <ekhangv@gmail.com>
2026-08-28 18:12:08 -0500

    Added creation of Ec2

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -7,3 +7,17 @@ resource "aws_key_pair" "main" {
         environment= "dev"
     }
 }
+
+resource "aws_instance" "main" {
+    ami                     = "ami-0d1f29d2140159d6d"
+    instance_type           = "t3.micro"
+    subnet_id               = aws_subnet.public.id
+    vpc_security_group_ids  = [aws_security_group.ec2.id]
+    key_name                = aws_key_pair.main.key_name
+    iam_instance_profile    = aws_iam_instance_profile.ec2.name
+    
+    tags = {
+        Project = "motorsport"
+        environment= "dev"
+    }
+}
