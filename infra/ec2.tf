458ca28ad9245dc785cc68b1d3d894202a0bc17b
dogaha <ekhangv@gmail.com>
2026-08-29 00:43:13 -0500

    Added docker onto ec2 instance

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -9,13 +9,23 @@ resource "aws_key_pair" "main" {
 }
 
 resource "aws_instance" "main" {
-    ami                     = "ami-0e508bdf5a1337e6b"
-    instance_type           = "t3.micro"
-    subnet_id               = aws_subnet.public.id
-    vpc_security_group_ids  = [aws_security_group.ec2.id]
-    key_name                = aws_key_pair.main.key_name
-    iam_instance_profile    = aws_iam_instance_profile.ec2.name
+    ami                         = "ami-0e508bdf5a1337e6b"
+    instance_type               = "t3.micro"
+    subnet_id                   = aws_subnet.public.id
+    vpc_security_group_ids      = [aws_security_group.ec2.id]
+    key_name                    = aws_key_pair.main.key_name
+    iam_instance_profile        = aws_iam_instance_profile.ec2.name
+    user_data_replace_on_change = true
     
+    user_data = <<-EOF
+        #!/bin/bash
+        dnf update -y
+        dnf install -y docker
+        systemctl enable docker
+        systemctl start docker
+        usermod -aG docker ec2-user
+    EOF
+
     tags = {
         Project = "motorsport"
         environment= "dev"
