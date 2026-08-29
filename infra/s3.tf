31090ca84599ba3c5163d99553e7f90e779cce5e
dogaha <ekhangv@gmail.com>
2026-08-29 01:07:32 -0500

    Added server side encryption to S3

--- infra/s3.tf
+++ infra/s3.tf
@@ -15,3 +15,13 @@ resource "aws_s3_bucket_public_access_block" "data_lake" {
     ignore_public_acls      = true
     restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "data_lake" {
+    bucket = aws_s3_bucket.data_lake.id
+
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
+}
