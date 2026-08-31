19ecfd83f99ddea87d1d9fcb482f86d11b1f9e75
dogaha <ekhangv@gmail.com>
2026-08-30 22:35:38 -0500

    Added rds instance

--- infra/rds.tf
+++ infra/rds.tf
@@ -25,3 +25,31 @@ resource "aws_secretsmanager_secret_version" "rds_credentials" {
         dbname      = "motorsport_database"
     })
 }
+
+resource "aws_db_parameter_group" "motorsport" {
+    name = "motorsport-postgres17"
+    family = "postgres17"
+
+    tags = {
+        Project     = "motorsport"
+        Environment = "dev"
+    }
+}
+
+resouce "aws_db_instance" "motorsport" {
+    identifier = "motorsport-postges"
+    engine = "postgres"
+    engine_version = "17.11"
+
+    instance_class = "db.t4g.micro"
+    allocated_storrage = 20
+    storage_type = "gp3"
+
+    db_name = "motorsport_database"
+    username = "motorsport_master"
+    password = random_password.rds_master.result
+
+    db_subnet_group_name = aws_db_subnet_group.motorsport.name
+    vpc_security_group_ids = [aws_security_group.rds.id]
+    parameter_group_name = aws_db_parameter_group.motorsport.name
+}
