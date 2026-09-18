4ce1267d5319b168049c1411e0f73b19f4966b6d
Ethan Vu <ekhangv@gmail.com>
2026-09-18 18:24:14 -0500

    Added Errors to telemetry: Nulls, Spikes, Retry, Blind-spots

--- source/telemetry.py
+++ source/telemetry.py
@@ -5,6 +5,7 @@ import json
 import boto3
 import random
 import psycopg2
+from collections import deque
 from psycopg2 import sql
 import numpy as np
 import pyarrow as pa
@@ -96,10 +97,12 @@ def batch_data(buffer: io.BytesIO,session_id:str):
 # Stream Live data
 def stream_data(data:dict,n:int,nth:int):
     # producer
-    producer = Producer({
-        "bootstrap.servers":"kafka:9092"
-    })
-    
+    producer = Producer({ "bootstrap.servers":"kafka:9092" })
+    bs_records = []
+    retry_records = deque()
+    in_blind_spot = False
+    blind_spot_ticks = 0
+
     for i in range(0,n,nth):
         record = {
             field: (
@@ -109,14 +112,79 @@ def stream_data(data:dict,n:int,nth:int):
             )
             for field in constants.LIVE_FIELDS
         }
+        payload = json.dumps(record).encode("utf-8")
+        
+        # Skip Row
+        if random.random() < 0.02:
+            time.sleep(1/constants.LIVE_HZ)
+            continue
 
-        # Push into kafka
-        producer.produce(
-            "telemetry",
-            value=json.dumps(record).encode("utf-8")
-        )
+        # blind spot
+        if random.random() < 0.03 and not in_blind_spot:
+            in_blind_spot = True
+            blind_spot_ticks = random.randint(5,15)
+
+        # retry error
+        if random.random() < 0.03:
+            retry_records.append(payload)
+            time.sleep(1/constants.LIVE_HZ)
+            continue
+
+        # Blind spot condition
+        if in_blind_spot:
+            # fill blind spot
+            bs_records.append(payload)
+            blind_spot_ticks -= 1
+            if blind_spot_ticks <= 0:
+                in_blind_spot = False
+        else:
+            # Push current payload
+            producer.produce(
+                "telemetry",
+                value=payload
+            )
+
+            # duplicate
+            if random.random() < 0.02:
+                producer.produce(
+                    "telemetry",
+                    value=payload
+                )
+
+            # burst refill
+            if bs_records:
+                random.shuffle(bs_records)
+                for p in bs_records:
+                    producer.produce(
+                        "telemetry",
+                        value=p
+                    )
+                bs_records.clear()
+
+            # retry past records
+            if retry_records and random.random() < 0.2:
+                producer.produce(
+                    "telemetry",
+                    value=retry_records.popleft()
+                )
         producer.poll(0)
         time.sleep(1/constants.LIVE_HZ)
+    # burst refill
+    if bs_records:
+        random.shuffle(bs_records)
+        for p in bs_records:
+            producer.produce(
+                "telemetry",
+                value=p
+            )
+        bs_records.clear()
+    while retry_records:
+        producer.produce(
+            "telemetry",
+            value=retry_records.popleft()
+        )
+        producer.poll(0)
+
     producer.flush()
     print("Finish Streaming Data")
     
