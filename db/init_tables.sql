b1dfda5b502029f30c783df3ea3e986c0ea1485e
dogaha <ekhangv@gmail.com>
2026-09-19 01:12:23 -0500

    Changed init tables to CREAT TABLE IF NOT EXIST and not DROP then Create

--- db/init_tables.sql
+++ db/init_tables.sql
@@ -1,5 +1,4 @@
-DROP TABLE IF EXISTS tracks CASCADE;
-CREATE TABLE tracks (
+CREATE TABLE IF NOT EXISTS tracks (
     track_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     state VARCHAR(20) NOT NULL,
@@ -9,8 +8,7 @@ CREATE TABLE tracks (
     end_coordinates POINT NOT NULL
 );
 
-DROP TABLE IF EXISTS track_turns CASCADE;
-CREATE TABLE track_turns (
+CREATE TABLE IF NOT EXISTS track_turns (
     turn_id SERIAL PRIMARY KEY,
     turn_number INT NOT NULL,
     track_id INT NOT NULL REFERENCES tracks(track_id),
@@ -18,51 +16,4 @@ CREATE TABLE track_turns (
     coordinate POINT,
 
     UNIQUE (track_id, turn_number)
-);
-
-DROP TABLE IF EXISTS drivers CASCADE;
-CREATE TABLE drivers (
-    driver_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    dob DATE,
-    weight INT
-);
-
-DROP TABLE IF EXISTS vehicles CASCADE;
-CREATE TABLE vehicles (
-    vehicle_id SERIAL PRIMARY KEY,
-    owner_id INT NOT NULL REFERENCES drivers(driver_id),
-    make VARCHAR(15),
-    model VARCHAR(15),
-    horsepower INT,
-    torque INT,
-    redline INT,
-    engine_layout VARCHAR(20),
-    engine_cylinders INT,
-    engine_displacement INT,
-    force_induction VARCHAR(15),
-    boost_pressure INT,
-    gearbox_type VARCHAR(10),
-    gear_count INT,
-    drivetrain VARCHAR(3),
-    length INT,
-    width INT,
-    height INT,
-    wheelbase INT,
-    wheel_diameter INT,
-    wheel_width INT,
-    wheel_weight INT,
-    curb_weight INT,
-    tires VARCHAR(20)
-);
-
-DROP TABLE IF EXISTS sessions CASCADE;
-CREATE TABLE sessions (
-    session_id UUID PRIMARY KEY,
-    track_id INT REFERENCES tracks(track_id),
-    vehicle_id INT REFERENCES vehicles(vehicle_id),
-    driver_id INT REFERENCES drivers(driver_id),
-    start_time TIMESTAMP DEFAULT NOW(),
-    end_time TIMESTAMP
 );
