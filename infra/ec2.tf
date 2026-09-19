ed7321483dc43a6c3923be49b787a52a260a6d36
dogaha <ekhangv@gmail.com>
2026-09-18 22:51:31 -0500

    Added dependecy (EC2 depends on RDS) and updated user_data command

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -15,15 +15,31 @@ resource "aws_instance" "main" {
     vpc_security_group_ids      = [aws_security_group.ec2.id]
     key_name                    = aws_key_pair.main.key_name
     iam_instance_profile        = aws_iam_instance_profile.ec2.name
+    depends_on = [aws_db_instance.motorsport]
+
     user_data_replace_on_change = true
-    
+    metadata_options {
+       http_put_response_hop_limit = 2
+       http_tokens                 = "required"
+    }
+
     user_data = <<-EOF
         #!/bin/bash
+        set -euxo pipefail
+
         dnf update -y
-        dnf install -y docker
-        systemctl enable docker
-        systemctl start docker
+        dnf install -y docker git
+        systemctl enable --now docker
         usermod -aG docker ec2-user
+
+        mkdir -p /usr/local/lib/docker/cli-plugins
+        curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+        curl -SL https://github.com/docker/buildx/releases/download/v0.37.1/buildx-v0.37.1.linux-amd64 -o /usr/local/lib/docker/cli-plugins/docker-buildx
+        chmod +x /usr/local/lib/docker/cli-plugins/*
+
+        git clone https://github.com/dogaha/motorsport /opt/motorsport
+        cd /opt/motorsport
+        docker compose up -d --build
     EOF
 
     tags = {
